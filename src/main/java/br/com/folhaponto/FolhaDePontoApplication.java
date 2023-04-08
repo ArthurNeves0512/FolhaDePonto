4281547f92b4f213d6873680efa0403615c1788b
arthur <arthur@pop-os.localdomain>
2023-04-08 13:38:43 -0300

    arquivos iniciais

---
+++ src/main/java/br/com/folhaponto/FolhaDePontoApplication.java
@@ -0,0 +1,13 @@
+package br.com.folhaponto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FolhaDePontoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FolhaDePontoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/folhaponto/FolhaDePontoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.folhaponto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FolhaDePontoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
